0e410e48fca22607634c7ce902a5a56abd1b02ba
ShengXue97 <simshengxue1997@gmail.com>
2020-12-21 19:10:29 +0800

    add basic home screen

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -73,6 +73,14 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float pointer_x = Input.GetAxisRaw("Horizontal");
+        float pointer_y = Input.GetAxisRaw("Vertical");
+        // if (Input.touchCount > 0)
+        // {
+        //     pointer_x = Input.touches[0].deltaPosition.x;
+        //     pointer_y = Input.touches[0].deltaPosition.y;
+        // }
+
         if (!meshRenderer.isVisible && Time.timeSinceLevelLoad > 0.1)
         {
             //Dead if player is not visible by any cameras
@@ -81,13 +89,13 @@ public class CharacterMovement : MonoBehaviour
             SceneManager.LoadScene(scene.name);
         }
 
-        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 0f)
+        if (Mathf.Abs(pointer_x) == 0f)
         {
             //Prevents holding down arrow keys to move, must release
             canMoveHorizontal = true;
         }
 
-        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 0f)
+        if (Mathf.Abs(pointer_y) == 0f)
         {
             //Prevents holding down arrow keys to move, must release
             canMoveVertical = true;
@@ -115,12 +123,12 @@ public class CharacterMovement : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, 200f * Time.deltaTime);
         if (Vector3.Distance(transform.position, movePoint.position) <= 0.05F)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f && canMoveHorizontal)
+            if (Mathf.Abs(pointer_x) == 1f && canMoveHorizontal)
             {
                 canMoveHorizontal = false;
-                if (Physics.OverlapSphere(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal") * 2, 0f, 0f), 0.2f, whatStopsMovement).Length == 0)
+                if (Physics.OverlapSphere(movePoint.position + new Vector3(pointer_x * 2, 0f, 0f), 0.2f, whatStopsMovement).Length == 0)
                 {
-                    if (Input.GetAxisRaw("Horizontal") == -1f)
+                    if (pointer_x == -1f)
                     {
                         gameObject.transform.rotation = Quaternion.Euler(0, -90, 0);
                     }
@@ -129,21 +137,21 @@ public class CharacterMovement : MonoBehaviour
                         gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
                     }
                     anim.SetBool("Hop", true);
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal") * 2, 0f, 0f);
+                    movePoint.position += new Vector3(pointer_x * 2, 0f, 0f);
                 }
-                if (Physics.OverlapSphere(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal") * 2, 0f, 0f), 0.2f, vehicleLayer).Length != 0)
+                if (Physics.OverlapSphere(movePoint.position + new Vector3(pointer_x * 2, 0f, 0f), 0.2f, vehicleLayer).Length != 0)
                 {
                     //EditorUtility.DisplayDialog("Info", "You died", "Ok");
                     Scene scene = SceneManager.GetActiveScene();
                     SceneManager.LoadScene(scene.name);
                 }
             }
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f && canMoveVertical)
+            else if (Mathf.Abs(pointer_y) == 1f && canMoveVertical)
             {
                 canMoveVertical = false;
-                if (Physics.OverlapSphere(movePoint.position + new Vector3(0f, 0f, Input.GetAxisRaw("Vertical") * 2), 0.2f, whatStopsMovement).Length == 0)
+                if (Physics.OverlapSphere(movePoint.position + new Vector3(0f, 0f, pointer_y * 2), 0.2f, whatStopsMovement).Length == 0)
                 {
-                    if (Input.GetAxisRaw("Vertical") == -1f)
+                    if (pointer_y == -1f)
                     {
                         zPos -= 1;
                         gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
@@ -166,9 +174,9 @@ public class CharacterMovement : MonoBehaviour
                         gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
                     }
                     anim.SetBool("Hop", true);
-                    movePoint.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical") * 2);
+                    movePoint.position += new Vector3(0f, 0f, pointer_y * 2);
                 }
-                if (Physics.OverlapSphere(movePoint.position + new Vector3(0f, 0f, Input.GetAxisRaw("Vertical") * 2), 0.2f, vehicleLayer).Length != 0)
+                if (Physics.OverlapSphere(movePoint.position + new Vector3(0f, 0f, pointer_y * 2), 0.2f, vehicleLayer).Length != 0)
                 {
                     //EditorUtility.DisplayDialog("Info", "You died", "Ok");
                     Scene scene = SceneManager.GetActiveScene();
