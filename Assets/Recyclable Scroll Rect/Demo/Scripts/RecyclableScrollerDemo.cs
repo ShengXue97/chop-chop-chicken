1e634e773e76850e19ce18b4df69061189162578
ShengXue97 <simshengxue1997@gmail.com>
2020-12-22 17:40:16 +0800

    add leaderboard

---
+++ Assets/Recyclable Scroll Rect/Demo/Scripts/RecyclableScrollerDemo.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using PolyAndCode.UI;
+
+/// <summary>
+/// Demo controller class for Recyclable Scroll Rect. 
+/// A controller class is responsible for providing the scroll rect with datasource. Any class can be a controller class. 
+/// The only requirement is to inherit from IRecyclableScrollRectDataSource and implement the interface methods
+/// </summary>
+
+//Dummy Data model for demostraion
+public class ContactInfo
+{
+    public string Name;
+    public string Email;
+    public string Score;
+    public string id;
+}
+
+public class RecyclableScrollerDemo : MonoBehaviour, IRecyclableScrollRectDataSource
+{
+    [SerializeField]
+    RecyclableScrollRect _recyclableScrollRect;
+
+    [SerializeField]
+    private int _dataLength;
+
+    //Dummy data List
+    private List<ContactInfo> _contactList = new List<ContactInfo>();
+
+    //Recyclable scroll rect's data source must be assigned in Awake.
+    private void Awake()
+    {
+        _recyclableScrollRect.DataSource = this;
+    }
+
+    static int SortByScore(ContactInfo p1, ContactInfo p2)
+    {
+        int p1Score = int.Parse(p1.Score);
+        int p2Score = int.Parse(p2.Score);
+        return p2Score.CompareTo(p1Score);
+    }
+    //Initialising _contactList with dummy data 
+    public void InitData(string data)
+    {
+        if (_contactList != null) _contactList.Clear();
+
+        string[] users = data.Split(';');
+
+        for (int i = 0; i < users.Length; i++)
+        {
+            string user = users[i];
+            string[] details = user.Split(',');
+
+            ContactInfo obj = new ContactInfo();
+            obj.Name = details[0];
+            obj.Email = details[1];
+            obj.Score = details[2];
+            obj.id = "";
+            _contactList.Add(obj);
+        }
+
+        _contactList.Sort(SortByScore);
+
+        for (int i = 0; i < _contactList.Count; i++)
+        {
+            _contactList[i].id = (i + 1).ToString();
+        }
+
+    }
+
+    #region DATA-SOURCE
+
+    /// <summary>
+    /// Data source method. return the list length.
+    /// </summary>
+    public int GetItemCount()
+    {
+        return _contactList.Count;
+    }
+
+    /// <summary>
+    /// Data source method. Called for a cell every time it is recycled.
+    /// Implement this method to do the necessary cell configuration.
+    /// </summary>
+    public void SetCell(ICell cell, int index)
+    {
+        //Casting to the implemented Cell
+        var item = cell as DemoCell;
+        item.ConfigureCell(_contactList[index], index);
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/AzureControl.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using UnityEngine;
+using Random = System.Random;
+using System.Collections;
+public class AzureControl : MonoBehaviour
+{
+    public CloudStorageAccount StorageAccount;
+    public string value;
+    public string result = "";
+    // Use this for initialization
+    void Start()
+    {
+        //BlobStorageTest ();
+        StartDownload("score.txt");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void StartUpload(string dest, string text)
+    {
+        StartCoroutine(AzureUpload(dest, text));
+    }
+    public void StartDownload(string dest)
+    {
+        StartCoroutine(AzureDownload(dest));
+    }
+
+    private IEnumerator AzureDownload(string dest)
+    {
+        result = "";
+
+        // Download a blob to your file system
+
+        StorageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=http;AccountName=xue;AccountKey=Xp5+gHw5N4e9mAhYFpNS8WNNbFJ/XfuhOlhSTGSLsOzySDlsIOoeucjdixRidL+1JX6NkEka9Umq4QVHka9xFg==;EndpointSuffix=core.windows.net");
+
+        CloudBlobClient blobClient = StorageAccount.CreateCloudBlobClient();
+
+
+        CloudBlobContainer container = blobClient.GetContainerReference("ccc");
+
+        CloudBlockBlob blockBlob = container.GetBlockBlobReference(dest);
+        //print ("Downloading" + dest);
+
+        try
+        {
+            string text = blockBlob.DownloadText();
+            result = text;
+            print("Downloaded: " + text);
+            GetComponent<RecyclableScrollerDemo>().InitData(text);
+
+        }
+        catch (StorageException ex)
+        {
+            print("Error downloading");
+
+        }
+        yield break;
+    }
+    private IEnumerator AzureUpload(string dest, string text)
+    {
+        result = "";
+
+        // Create a blob client for interacting with the blob service.
+        StorageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=http;AccountName=xue;AccountKey=Xp5+gHw5N4e9mAhYFpNS8WNNbFJ/XfuhOlhSTGSLsOzySDlsIOoeucjdixRidL+1JX6NkEka9Umq4QVHka9xFg==;EndpointSuffix=core.windows.net");
+
+        CloudBlobClient blobClient = StorageAccount.CreateCloudBlobClient();
+
+
+        CloudBlobContainer container = blobClient.GetContainerReference("ccc");
+
+        CloudBlockBlob blockBlob = container.GetBlockBlobReference(dest);
+        //print ("Uploading" + dest + ";" + text);
+        try
+        {
+            blockBlob.UploadText(text);
+            result = "true";
+            //print("Uploaded");
+        }
+        catch (StorageException)
+        {
+            result = "false";
+            //print ("Error uploading");
+        }
+
+        yield break;
+    }
+
+    private string ConvertLastModifiedToSecocnds(string lastmodified)
+    {
+        string oneline = lastmodified.Split('\n')[0];
+        string onlytime = oneline.Split(' ')[1];
+        var timesplit = onlytime.Split(':');
+
+        int hour = int.Parse(timesplit[0]);
+        int minute = int.Parse(timesplit[1]);
+        int second = int.Parse(timesplit[2]);
+
+        string onlydate = oneline.Split(' ')[0];
+        var datesplit = onlydate.Split('/');
+
+        int day = int.Parse(datesplit[0]);
+        int month = int.Parse(datesplit[1]);
+        int year = int.Parse(datesplit[2]);
+
+        string finalSeconds = (second + (minute * 60) + (hour * 3600) + (day * 86400) + (month * 2.628e+6) + (year * 3.154e+7)).ToString();
+        return finalSeconds;
+    }
+}
